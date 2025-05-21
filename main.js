ac44108cc015de292782b9e87fdb9b4ad440ab10
zedeloc <desmondcole1990@gmail.com>
2025-05-20 20:49:46 -0700

    Create Gameboard object with gameboard array and updateGameboard() method

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 console.log("It works!")
+
+// Create Gameboard object with gameboard array
+// Create Player object
+// Create Game object that controls the game
+// Remember: Little to no global code. Use factories in general, and IIFE (module pattern) for single instances.
+// Consider carefully where each bit of logic goes
+// Use buttons for the board items, column attribute (0, 1, 2)
+
+function createGameboard () {
+    const gameboard = [
+        [undefined, undefined, undefined],[undefined, undefined, undefined],[undefined, undefined, undefined]
+    ];
+
+    const updateCell = (row, col, value) => {
+        gameboard[row].splice(col, 1, value);
+        console.log(`Row: ${row}, Column: ${col}: is now ${value}.`);
+
+        console.table(Gameboard.getGameboard());
+    }
+
+    const getGameboard = () => gameboard;
+    return { getGameboard, updateCell };
+};
+
+const Gameboard = createGameboard();
