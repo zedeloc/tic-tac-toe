68004da0faefce74a391c425dd0280e9ad945ca6
zedeloc <desmondcole1990@gmail.com>
2025-05-22 21:26:58 -0700

    Impliment makeMove() using prompt() to recieve player moves
    Impliment checkForWin() to find winning scenarios
    Impliment checkForDraw() to check for draw scenarios
    Impliment gameLoop() to allow the game to run in console

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log("It works!")
-
 // Create Gameboard object with gameboard array
 // Create Player object
 // Create Game object that controls the game
@@ -16,8 +14,8 @@ function createGameboard () {
         if (checkIfFree(row, col)) {
             gameboard[row].splice(col, 1, value);
         console.log(`Row: ${row}, Column: ${col}: is now ${value}.`);
-
-        console.table(Gameboard.getGameboard());
+            return true;
+        
         } else {
             return false;
         }  
@@ -50,17 +48,26 @@ function createPlayer (player) {
 }
 
 const game = (() => {
+    // make player objects, store in array, designate marks
+    active = true;
     const players = [
         createPlayer("player1"),
         createPlayer("player2")
     ];
     players[0].assignMark("x");
     players[1].assignMark("o");
-
+    // make gameboard and set active mark
     const gameboard = createGameboard();
     let activeMark = "x";
-
-
+    // let the user know what to do and show gameboard
+    function updateCurrentState() {
+        console.clear();
+        for (let i = 0; i < 6; i++) {
+            console.log(i)
+        }
+        console.log("To make a move call 'game.makeMove()' and please enter Row.Col separated by a single period")
+        console.table(gameboard.getGameboard());
+    }
 
     function getActivePlayer() {
         for (player of players) {
@@ -76,11 +83,82 @@ const game = (() => {
     };
 
     function makeMove() {
-        
-        console.log(" ")
+        console.log(`${getActivePlayer()}'s turn. Place mark: ${activeMark}`)
+        promptForMove()
+
+        function promptForMove() {
+            const move = prompt(`${getActivePlayer()} (${activeMark}), enter move as Row.Col separated by a single period`);
+            const moveArray = move.split('.')
+           const isValidMove = gameboard.updateCell(moveArray[0], moveArray[1], activeMark);
+           if (!isValidMove) {
+            console.log("invalid move. Lets try again");
+            makeMove()
+           } else {
+            console.table(gameboard.getGameboard());
+            switchActiveMark();
+            updateCurrentState();
+           }
+        }
+
+
     }
 
-    return { players, gameboard, getActivePlayer, switchActiveMark }
+    function checkForWin(mark) {
+        const boardToEvaluate = gameboard.getGameboard();
+        // check horizontal
+        for (row of boardToEvaluate) {
+        
+            if (row[0] === mark && row[1] === mark && row[2] === mark) {
+                console.log(`${mark} wins in row:`)
+                return true
+            }
+        }
+        // check vertical
+        for (let i = 0; i < 3; i++) {
+            if (boardToEvaluate[0][i] === mark && boardToEvaluate[1][i] === mark && boardToEvaluate[2][i] === mark) {
+                console.log(`${mark} wins! in column ${i+1}`)
+                return true
+            }
+        }
+        // check diagonal
+        if (boardToEvaluate[0][0] === mark && boardToEvaluate[1][1] === mark &&boardToEvaluate[2][2] === mark) {
+            console.log(`${mark} wins with diagonal 1`);
+            return true;
+        } else if (boardToEvaluate[0][2] === mark && boardToEvaluate[1][1] === mark && boardToEvaluate[2][0] === mark) {
+            console.log(`${mark} wins with diagonal 2`);
+            return true;
+        }
+        // check for draw
+        
+    }
+
+    function checkForDraw() {
+        const boardToEvaluate = gameboard.getGameboard();
+        for (row of boardToEvaluate) {
+            for (let i = 0; i < 3; i++) {
+                if (!row[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+
+    function gameLoop() {
+        while (active) {
+            
+            updateCurrentState()
+            checkForWin("x");
+            checkForWin('o');
+            checkForDraw();
+            makeMove()
+        }
+    }
+
+    gameLoop();
+
+    return { players, gameboard, getActivePlayer, switchActiveMark, makeMove }
 })();
 
 
